8b7137f671e9d157b630beb1f8804cd2c5a00d7f
Eric Tran <erictran1547@gmail.com>
2025-04-11 12:04:40 -0700

    Finished Video 270

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Petsoft - Pet daycare software",
+  description: "Take care of people's pets responsibly with PetSoft.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} text-sm text-zinc-900 bg-[#E5E8EC] min-h-screen`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
